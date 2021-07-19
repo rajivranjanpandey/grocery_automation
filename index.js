e6c9babaa7ef4ee72bd09d0ebe9a7f6f24686b7f
rajiv ranjan pandey <raj.pandey914@gmail.com>
2021-07-19 22:41:23 +0530

    [edit] working on add btn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const fastify = require('fastify');
 const fastifyFormBody = require('fastify-formbody');
-// const fastifyStatic = require('fastify-static');
 const path = require('path');
 const searchFilter = require('./filters');
 
@@ -9,9 +8,11 @@ app.register(require('fastify-pug'), { views: 'views' });
 app.register(fastifyFormBody);
 app.register(require('fastify-static'), { root: path.join(__dirname, './', 'assets') });
 
+
 app.get('/', (request, reply) => {
     reply.render('home.pug');
 });
+
 app.get('/assets/index.css', (request, reply) => {
     return reply.sendFile('index.css');
 })
@@ -24,8 +25,13 @@ app.post('/search', async (request, reply) => {
     reply
         .code(200)
         .render('search-result.pug', { searchRes });
+});
+app.post('/add', (request, reply) => {
+    const addReq = request.body;
+    console.log(addReq);
 })
+
+
 app.listen(3000, (err, address) => {
-    console.log(path.join(__dirname, 'assets'))
     console.log(err, address)
 })
